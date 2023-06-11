26530c1eb9a7055fddec1ebbdf5841f855f02d57
Ksenia <kseniakalinnikova268@Gmail.com>
2023-06-11 15:34:11 +0300

    first commit

---
+++ src/test/java/ru/netology/UIAutomatorTest.java
@@ -0,0 +1,54 @@
+package ru.netology;
+
+import io.appium.java_client.AppiumDriver;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UIAutomatorTest {
+    private AppiumDriver driver;
+
+    @BeforeEach
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("appium:deviceName", "Some name");
+        desiredCapabilities.setCapability("appium:appPackage", "ru.netology.testing.uiautomator");
+        desiredCapabilities.setCapability("appium:appActivity", "ru.netology.testing.uiautomator.MainActivity");
+        desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
+        desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
+        desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
+        desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
+
+        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+
+        driver = new AppiumDriver(remoteUrl, desiredCapabilities);
+    }
+
+
+    @Test
+    public void NewActivity() {
+        MainScreen button = new MainScreen(driver);
+        String newText = "Hello,UiAutomator";
+        button.userInput.sendKeys(newText);
+        button.buttonActivity.click();
+        button.activityText.isDisplayed();
+        Assertions.assertEquals(newText, button.activityText.getText());
+    }
+    @Test
+    public void SettingAnEmptyString() {
+        MainScreen button = new MainScreen(driver);
+        String textBefore = button.textToBeChanged.getText();
+        button.userInput.sendKeys(" ");
+        button.buttonChange.click();
+        String textAfter = button.textToBeChanged.getText();
+        Assertions.assertEquals(textBefore, textAfter);
+    }
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+}
